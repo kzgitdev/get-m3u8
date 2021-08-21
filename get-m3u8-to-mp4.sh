18d0b01879a557c50c276f50b5c8310a3578fad9
Kazuya.i <kzgitdev@outlook.com>
2021-08-21 22:53:27 +0900

    Create get-m3u8-to-mp4.sh

---
+++ get-m3u8-to-mp4.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+##
+# Usage:
+# ./gget-m3u8-to-mp4.sh URL OUTPUT
+# param1 : url of live streaming youtube program.
+# param2 : the file name output in mp4 format.
+#
+# ./get-m3u8.sh https://www.youtube.com/watch?v=-ABC123efg_%_ output.mp4
+##
+
+# URL is youtube live streaming video
+# https://www.youtube.com/watch?v=-ABC123efg_%_
+##
+URL=$1
+echo ${URL}
+
+##
+# NUM is number of the best quality format.
+# command youtube-dl --list-fromats...
+#
+# format code  extension  resolution note
+# 91           mp4        256x144     269k , avc1.4d400c, 30.0fps, mp4a.40.5
+# 92           mp4        426x240     507k , avc1.4d4015, 30.0fps, mp4a.40.5
+# 93           mp4        640x360     962k , avc1.4d401e, 30.0fps, mp4a.40.2
+# 94           mp4        854x480    1282k , avc1.4d401f, 30.0fps, mp4a.40.2
+# 95           mp4        1280x720   2447k , avc1.4d401f, 30.0fps, mp4a.40.2 (best)
+##
+NUM=`youtube-dl --list-formats "${URL}" | grep best | cut -d " " -f 1`
+
+##
+# get the *.m3u8 file.
+##
+M3U8=`youtube-dl -f ${NUM} -g ${URL}`
+
+##
+# 
+#
+##
+ffmpeg -i "${M3U8}" -c copy -bsf:a aac_adtstoasc "$2"
